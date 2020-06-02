50bebd41e217e1b65e089ced18229fdd3c5b8772
ismaelmk <ismael.ruben.pr@gmail.com>
2020-06-02 13:03:15 -0500

    Update compile_proto_java.sh

--- compile_proto_java.sh
+++ compile_proto_java.sh
@@ -5,6 +5,7 @@ unzip gradle-6.4.1-bin.zip
 
 printf "apply plugin: 'maven'
 apply plugin: 'java'
+apply plugin: 'maven-publish'
 
 group 'com.oyster.protos'
 version '1.0'
@@ -26,6 +27,21 @@ dependencies {
     compile group: 'io.grpc', name: 'grpc-netty-shaded', version: protobufVersion
     compile group: 'io.grpc', name: 'grpc-stub', version: protobufVersion
 }
+
+publishing {
+
+  repositories {
+    maven {
+      name = 'GitHubPackages'
+      url = 'https://maven.pkg.github.com/ismaelmk/proto_repo'
+      credentials = {
+        username = System.getenv('GITHUB_ACTOR')
+        password = System.getenv('GITHUB_TOKEN')
+      }
+    }
+  }
+}
+
 " > build.gradle
 ./gradle-6.4.1/bin/gradle build
 
